cf0eae1751c175ab122a163b88ba8eef9558dca0
Aylin Travnik <60603189+AylinTravnik@users.noreply.github.com>
2022-04-21 22:39:29 -0300

    Add files via upload

--- testViaje.php
+++ testViaje.php
@@ -1,13 +1,16 @@
-
-<?php
-//ENTREGA Nº 1: VIAJES
-//ALUMNOS: TRAVNIK ARMITANO, VALERIA AYLIN, FI3522
-//         ZUCCATO, STEFANO, FI3517
-
+<?php 
 include 'Viaje.php'; 
+include 'pasajero.php';
+include 'ResponsableV.php';
 
 $viaje = null;
 $opcion = 1;
+
+//DATOS PRE CARGADOS//
+$viaje= new Viaje("AA1234","China",23,[],"");
+
+
+
 //Menu principal
 while($opcion != 0){
     $menu = "\033[1mMenú de opciones:\033[0m";
@@ -19,67 +22,73 @@ while($opcion != 0){
     2-Cargar un pasajero
     3-Modificar informacion viaje
     4-Modificar Pasajero
-    5-Visualizar informacion del viaje
+    5-Agregar Informacion Responsable
+    6-Modificar Informacion Responsable
+    7-Visualizar informacion del viaje
     \n";
     $opcion = readline();
     switch($opcion){
         case 0 : echo "Saliendo del test"; break;
         case 1 : $viaje = crearViaje();break;
         case 2 :  if ($viaje !== null) {agregarPasajero($viaje);} else {echo "No se cargó un viaje.\n";};break;
-        case 3 : if ($viaje !== null) {modificarVuelo($viaje);} else {echo "No se cargó un viaje.\n";};break;
+        case 3 : if ($viaje !== null) {$viaje->modificarVuelo();} else {echo "No se cargó un viaje.\n";};break;
         case 4 : if ($viaje !== null) {$DNIPasajero = readline("Ingrese el DNI del pasajero a modificar: ");
-            $viaje->modificarPasajero($DNIPasajero);break;} else {echo "No se cargó un viaje.\n";};break;
-            
-        case 5: if ($viaje !== null) {echo $viaje;} else {echo "No se cargó un viaje.\n";};break;
+            $viaje->cambiarPasajero($DNIPasajero);break;} else {echo "No se cargó un viaje.\n";};break;
+        case 5:  if ($viaje !== null) {agregarResponsableV($viaje);} else {echo "No se cargó un viaje.\n";};break; 
+        case 6:  if ($viaje !== null) {$viaje->modificarResponsable();} else {echo "No se cargó un viaje.\n";};break;
+        case 7: if ($viaje !== null) {echo $viaje; $viaje->imprimirPasajeros();} else {echo "No se cargó un viaje.\n";};break;
     }
 }
 
-//Funcion modificar vuelo, Un menu, permite Modificar individualmente el Codigo de vuelo, destino y la cantidad maxima de pasajeros.
-function modificarVuelo($viaje){
-    $opcion = 1;
-    while($opcion != 0){
-        echo "
-        \n
-        0-Salir 
-        1-Modificar Codigo de vuelo 
-        2-Modificar Destino
-        3-Modificar Cantidad Maxima Pasajeros
-        \n";
-        $opcion = readline();
-        echo "\n";
-        switch($opcion){
-            case 0 : echo "Saliendo del test"; break;
-            case 1 : $cambiarCodigo = readline("Ingrese el nuevo codigo de vuelo: ");
-                     $viaje->setCodigoVuelo($cambiarCodigo); break;
-            case 2 : $cambiarDestino = readline("Ingrese el nuevo destino de vuelo: ");
-                     $viaje->setDestino($cambiarDestino); break;
-            case 3 :  $cambiarCantidad = readline("Ingrese la nueva cantidad maxima de pasajeros: ");
-                     $viaje->setCantidadPasajeros($cambiarCantidad); break;
-                    }
-    }                
-}
-//Funcion Crear Viaje, Crea un Objeto de la Clase Viaje, con los datos entrados por el usuario
+//Funcion Crear viaje, Crea un Objeto de la Clase viaje, con los datos entrados por el usuario
 function crearViaje(){
+  $responsable= "";  
   $pasajeros = [];
   $codigo= readline("Ingrese el codigo de vuelo: ");
   $destino = readline("Ingrese el destino: ");
   $capacidad = readline("Ingrese la capacidad del vuelo: ");
-  $nuevoViaje = new Viaje($codigo, $destino, $capacidad, $pasajeros);
+  $nuevoViaje = new Viaje($codigo, $destino, $capacidad, $pasajeros,$responsable);
   echo "Se creo correctamente un nuevo viaje.\n ";
   return $nuevoViaje;
 }
 
-//Permite agregar nuevos pasajeros al viaje, Controla que no se supere el maximo permitido
-function agregarPasajero($nuevoViaje){
-    $nuevoViaje;
-    if (count($nuevoViaje->getColeccionPasajeros())+1<=$nuevoViaje->getCantidadPasajeros()){
-   $nombrePasajero = readline("Ingrese Nombre del Pasajero: ");
-   $apellidoPasajero = readline("Ingrese Apellido del pasajero: ");
-   $dniPasajero= readline("Ingrese Dni del pasajero: ");
-    return $nuevoViaje->populatePasajeros($nombrePasajero, $apellidoPasajero, $dniPasajero);
-} else {
-    echo "El vuelo está lleno.\n";
+function agregarPasajero($viaje){
+    //**********DATOS PRECARGADOS***************/
+    $pasajero1= new Pasajero("Luis","Saenz", 40123123,4465444);
+    $viaje->agregarPasajeroAColeccion($pasajero1);
+    $pasajero2= new Pasajero("Pepe","Rodriguez", 30123123,4465900);
+    $viaje->agregarPasajeroAColeccion($pasajero2);
+    $pasajero3= new Pasajero("Luna","Perez", 20123123,4465555);
+    $viaje->agregarPasajeroAColeccion($pasajero3);
+    $pasajero4= new Pasajero("Maria","Saenz", 10123123,4465444);
+    $viaje->agregarPasajeroAColeccion($pasajero4);
+
+    // PARA PROBAR LA FUNCION; DES COMENTARLA//
+
+    /* $nombrePasajero = readline("Ingrese Nombre del Pasajero: ");
+    $apellidoPasajero = readline("Ingrese Apellido del pasajero: ");
+    $dniPasajero= readline("Ingrese Dni del pasajero: ");
+    $telefonoPasajero=readline("Ingrese el telefono del pasajero:");
+    $nuevoPasajero= new Pasajero($nombrePasajero,$apellidoPasajero,$dniPasajero,$telefonoPasajero);
+    $viaje->agregarPasajeroAColeccion($nuevoPasajero); */
+
 }
+
+function agregarResponsableV($viaje){
+    //**********DATOS PRECARGADOS********** */
+    $responsable= new ResponsableV( "Lucia", "Aguilar",3453, 4325);
+    $viaje->setResponsable($responsable);
+ 
+    // PARA PROBAR LA FUNCION; DES COMENTARLA//
+
+    /* $nombreResponsable = readline("Ingrese Nombre del Responsable: ");
+    $apellidoResponsable = readline("Ingrese Apellido del Responsable: ");
+    $numeroLicencia= readline("Ingrese numero de licencia del responsable: ");
+    $numeroEmpleado=readline("Ingrese el numero de Empleado del responsable:");
+    $nuevoResponsable= new ResponsableV($nombreResponsable,$apellidoResponsable,$numeroLicencia,$numeroEmpleado);
+    $viaje->setResponsable($nuevoResponsable); */
+
+
 }
-       
-?>       
+
+
